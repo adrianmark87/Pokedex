9f95864ec2df4593c4c5d5b86deca11ec01de134
Adrian Marculescu <adrianmarculescu.ing@gmail.com>
2023-03-29 16:53:17 +0200

    Pokedex - Challenge 3

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,22 @@
 
-
-
-
 function PokemonCard() {
+    const pokemonList = [
+        {
+          name: "bulbasaur",
+          imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        },
+        {
+          name: "mew",
+        },
+      ];
+      const pokemon = pokemonList[0]
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="Pokemon" />
-            <figcaption>bulbasaur</figcaption>
+            {pokemon.imgSrc === "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+            ? <img src={pokemon.imgSrc} alt="Pokemon" /> : <p>???</p> }
+          <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 }
-export default PokemonCard;
+export default PokemonCard; 
